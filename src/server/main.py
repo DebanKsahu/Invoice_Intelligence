e8e4fd67a607633f91b9f2ef75f5aefd1981d014
Deban Kumar Sahu <sahudebankumar@gmail.com>
2026-03-10 18:48:55 +0530

    feat(app): initialize application with settings and middleware
    Set up the FastAPI application with necessary middleware for CORS
    and session management. Integrated application dependency
    injection with settings and database session management.

--- src/server/main.py
+++ src/server/main.py
@@ -1,9 +1,15 @@
 from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
+from starlette.middleware.sessions import SessionMiddleware
 
 from core.AppDependency import AppDependency
 from internal.auth.handler import createAuthRouter
+from internal.gmail.handler import createGmailRouter
+from internal.platform.config.Settings import Settings
+from internal.platform.database.Dependency import createAsyncSessionMaker
+from internal.platform.database.PostgresSQLSetup import createAsyncEngine
 
 
 @asynccontextmanager
@@ -11,9 +17,25 @@ async def appLifespan(app: FastAPI):
     yield
 
 
-app = FastAPI(debug=True, version="0.1.0", title="", lifespan=appLifespan)
-applicationDependencies: AppDependency = app.state.di
-app.include_router(createAuthRouter(applicationDependency=applicationDependencies))
+settings = Settings()
+asyncEngine = createAsyncEngine(settings=settings)
+asyncSessionMaker = createAsyncSessionMaker(asyncEngine=asyncEngine)
+applicationDependency = AppDependency(
+    settings=settings, asyncEngine=asyncEngine, asyncSessionMaker=asyncSessionMaker
+)
 
-if __name__ == "__main__":
-    pass
+
+app = FastAPI(debug=True, version="0.1.0", title="Invoice Intelligence", lifespan=appLifespan)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.add_middleware(SessionMiddleware, secret_key="secret")
+
+app.include_router(createAuthRouter(applicationDependency=applicationDependency))
+app.include_router(createGmailRouter(applicationDependency=applicationDependency))
